2ec53d5d5164efa45bf35a67398c0450511f7d87
Anish Kshatriya <obliviga@gmail.com>
2019-05-15 20:09:57 -0700

    resolved FOUC

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,92 +1,71 @@
 const path = require('path');
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-    devtool: 'eval-cheap-module-source-map',
-    entry: './src/js/index.js',
-    devServer: {
-        port: 8080,
-        contentBase: path.join(__dirname, "dist")
-    },
-    node: {
-        fs: 'empty'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                options: {
-                    presets: ['env']
-                }
+  devtool: 'eval-cheap-module-source-map',
+  entry: './src/js/index.js',
+  devServer: {
+    port: 8080,
+    contentBase: path.join(__dirname, 'dist'),
+  },
+  node: {
+    fs: 'empty',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['env'],
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            'css-loader',
+            'sass-loader',
+          ],
+        }),
+      },
+      {
+        // Load all images as base64 encoding if they are smaller than 8192 bytes
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              // On development we want to see where the file is coming from, hence we preserve the [path]
+              name: '[path][name].[ext]?hash=[hash:20]',
+              limit: 8192,
             },
-            {
-                test: /\.(scss|css)$/,
-                use: [
-                    {
-                        // creates style nodes from JS strings
-                        loader: "style-loader",
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    {
-                        // translates CSS into CommonJS
-                        loader: "css-loader",
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    {
-                        // compiles Sass to CSS
-                        loader: "sass-loader",
-                        options: {
-                            outputStyle: 'expanded',
-                            sourceMap: true,
-                            sourceMapContents: true
-                        }
-                    }
-                    // Please note we are not running postcss here
-                ]
-            }
-            ,
-            {
-                // Load all images as base64 encoding if they are smaller than 8192 bytes
-                test: /\.(png|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            // On development we want to see where the file is coming from, hence we preserve the [path]
-                            name: '[path][name].[ext]?hash=[hash:20]',
-                            limit: 8192
-                        }
-                    }
-                ]
-            }
-            ,
-            {
-                // Load all icons
-                test: /\.(eot|woff|woff2|svg|ttf)([\?]?.*)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    }
-                ]
-            }
+          },
         ],
+      },
+      {
+        // Load all icons
+        test: /\.(eot|woff|woff2|svg|ttf)([\?]?.*)$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      jquery: 'jquery/src/jquery',
     },
-    resolve: {
-      alias: {
-        jquery: "jquery/src/jquery"
-      }
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html',
-            inject: true
-        })
-    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      inject: true,
+    }),
+    new ExtractTextPlugin('styles.css'),
+  ],
 };
