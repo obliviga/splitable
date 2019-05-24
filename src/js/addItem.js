531e0a165cf9f86b2cc02030e39791ed6c68e5a6
Anish Kshatriya <obliviga@gmail.com>
2019-05-24 15:37:06 -0700

    each item for each person has the correct index.

--- src/js/addItem.js
+++ src/js/addItem.js
@@ -1,17 +1,22 @@
+import getItemQuantity from './utils/getItemQuantity';
+import getPeopleQuantity from './utils/getPeopleQuantity';
+
 $(document).ready(() => {
-  let personIndex = 1;
+  let itemIndex = 1;
+  const itemQuantity = getItemQuantity();
+  const peopleQuantity = getPeopleQuantity();
 
   // Add a person with a consecutive index starting from 2
   $('#addItem').click(() => {
-    $(`th:contains("Item ${personIndex}")`).after(
-      `<th>Item ${personIndex + 1} Price</th>`,
+    $(`th:contains("Item ${itemIndex}")`).after(
+      `<th>Item ${itemIndex + 1} Price</th>`,
     );
 
-    personIndex += 1;
+    itemIndex += 1;
 
     $('tr td:last-child').after(
       `<td>
-        <input type="number" class="item0" min="0"/>
+        <input type="number" class="person${1} item${itemIndex}" min="0" />
       </td>`,
     );
   });
