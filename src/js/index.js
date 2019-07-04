4ab0ca822a2c877a2221d6c07af6a37123394914
Anish Kshatriya <obliviga@gmail.com>
2019-07-03 20:34:34 -0700

    more automatic linting, and adding issues link in package.json

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
+import Splitable from './splitable';
+
 // Styles
 require('normalize.css/normalize.css');
 require('./../styles/index.scss');
-
-import Splitable from  './splitable';
 
 const splitable = new Splitable('#splitable');
 
