0e3a5bda301ec56f08eca482b538c7319096545a
Anish Kshatriya <obliviga@gmail.com>
2019-09-12 17:54:48 -0700

    linting webpack file. applying styles to table. removing unused files. fixing inputs so the labels render correctly. removing build files. changing background color. making warning text bold

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -7,15 +7,16 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'cheap-eval-source-map',
   output: {
-    chunkFilename: 'js/[name].chunk.js'
+    chunkFilename: 'js/[name].chunk.js',
   },
   devServer: {
-    inline: true
+    inline: true,
+    host: '192.168.0.111',
   },
   plugins: [
     new Webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
-    })
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    }),
   ],
   module: {
     rules: [
@@ -26,17 +27,17 @@ module.exports = merge(common, {
         loader: 'eslint-loader',
         options: {
           emitWarning: true,
-        }
+        },
       },
       {
         test: /\.(js)$/,
         include: Path.resolve(__dirname, '../src'),
-        loader: 'babel-loader'
+        loader: 'babel-loader',
       },
       {
         test: /\.s?css$/i,
-        use: ['style-loader', 'css-loader?sourceMap=true', 'sass-loader']
-      }
-    ]
-  }
+        use: ['style-loader', 'css-loader?sourceMap=true', 'sass-loader'],
+      },
+    ],
+  },
 });
